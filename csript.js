9cb7cff2a081812518338e9bc8cf392b25012099
NazariySokol94 <113379693+NazariySokol94@users.noreply.github.com>
2023-01-29 16:18:24 +0300

    Add files via upload

---
+++ csript.js
@@ -0,0 +1,82 @@
+let a = ""; // first number
+let b = ""; // cesoct number
+let sign = ""; // знак операции
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["-", "+", "X", "/"];
+
+// экран
+const out = document.querySelector(".calc-screen p");
+function clearAll() {
+  a = ""; // fiest number and result
+  b = ""; // second number
+  sign = ""; // знак
+finish = false;
+out.textContent = 0;
+}
+document.querySelector(".ac").onclick = clearAll;
+
+document.querySelector(".buttons").onclick = (Event) => {
+  // нажата не кнопка
+if (!Event.target.classList.contains("btn")) return;
+  // нажата кнопка clearAll ac
+if (Event.target.classList.contains("ac")) return;
+out.textContent = "";
+
+  // получаю нажатую кнопку
+const key = Event.target.textContent;
+
+  // если нажата кнпка 0-9
+if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+    a += key;
+
+    out.textContent = a;
+    } else if (a !== "" && b !== "" && finish) {
+    b = key;
+    finish = false;
+    out.textContent = b;
+    } else {
+    b += key;
+    out.textContent = b;
+    }
+    console.table(a, b, sign);
+}
+  //  если нажата кнопка + - / *
+if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.table(a, b, sign);
+    return;
+}
+
+  //  нажат кнопка =
+if (key === "=") {
+    if (b === "") b = a;
+    switch (sign) {
+    case "+":
+        a = +a + +b;
+        break;
+    case "-":
+        a = a - b;
+        break;
+    case "X":
+        a = a * b;
+        break;
+    case "/":
+        if (b === "0") {
+          out.textContent = " валенок";
+          a = "";
+          b = "";
+          sign = "";
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.table(a, b, sign);
+}
+};
